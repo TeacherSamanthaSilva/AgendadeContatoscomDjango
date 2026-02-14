30fc7e0deb8e9c055ccd1217e6a768a23fbe8dd1
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-02-13 22:15:50 -0300

    Update views.py

--- contatos/views.py
+++ contatos/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import Contato
+from .forms import ContatoForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def listar_contatos(request):
+    busca = request.GET.get('q')
+    contatos = Contato.objects.filter(usuario=request.user)
+
+    if busca:
+        contatos = contatos.filter(nome__icontains=busca)
+
+    return render(request, 'contatos/lista.html', {'contatos': contatos})
