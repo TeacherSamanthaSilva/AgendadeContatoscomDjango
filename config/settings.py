175fe38c44310a659cb618eb15602890d6ac3432
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-02-13 22:29:13 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contatos',
 ]
 
 MIDDLEWARE = [
@@ -119,11 +120,6 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-INSTALLED_APPS = [
-    ...
-    'contatos',
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
