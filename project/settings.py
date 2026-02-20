3bdb06ba9c456e103e7b66bf1261f9f4e54f5aba
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-02-19 23:38:51 -0300

    arquivos

--- project/settings.py
+++ project/settings.py
@@ -112,7 +112,7 @@ LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
-USE_I18N = False
+USE_I18N = True
 
 USE_TZ = True
 
@@ -133,3 +133,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
